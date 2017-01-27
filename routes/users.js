9332cc69a1a96115f5207fc6a4e569f1e5aae9bc
cresta white <crestawhite@gmail.com>
2017-01-26 20:52:36 -0500

    add delete function

--- routes/users.js
+++ routes/users.js
@@ -15,4 +15,23 @@ router.get('/new', function(req, res, next) {
   res.render('users/new', { title: 'new fazbook users' });
 });
 
+router.post('/', function(req, res, next) {
+  models.User.create({
+    email: req.body.email,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    dob: req.body.dob
+  }).then(function(users) {
+    res.redirect('/users');
+  });
+});
+ router.delete('/:id', function(req, res, next) {
+  models.User.destroy ({
+    where: {id: req.params.id}
+  }).then(function(user) {
+    res.redirect('/users');
+  });
+ });
+
+
 module.exports = router;
