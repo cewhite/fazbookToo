1e8bb2189e447bbcfd65e6fb29684265f8b0859d
cresta white <crestawhite@gmail.com>
2017-01-26 20:16:01 -0500

    draft working to GET

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
+var models = require('../server/models/index');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('users/index', { title: 'fazbook' });
+router.get('/', function(req, res,next) {
+  models.User.findAll( { } ).then(function(users) {
+    res.render('users/index', {
+      title: 'all fazbook users',
+      users:users
+    });
+  });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('users/new', { title: 'new fazbook users' });
 });
 
 module.exports = router;
